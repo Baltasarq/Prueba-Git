0e300ca073126360a471264f43165b4a10c650e3
Baltasar <baltasarq@gmail.com>
2023-11-03 12:18:27 +0100

    Cambia "programa" por "App"

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Prueba_Git;
 
 var Muestra = (string m) => Consola.Print( m );
 
-Muestra( "Este programa solo tiene interfaz de consola." );
+Muestra( "Esta app solo tiene interfaz de consola." );
 Muestra( "Hola desde Rider, ¡¡probando Git!!" );
 Muestra( "Parece que funciona y es una herramienta útil." );
 
