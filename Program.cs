036f3695011c42d7d0a8b493fda91ee0b9fae4ae
Baltasar <baltasarq@gmail.com>
2023-11-03 12:10:25 +0100

    Vuelta a System.Console

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿
 
-using Prueba_Git;
-
-Consola.Print( "Este programa solo tiene interfaz de consola." );
-Consola.Print( "Hola desde Rider, ¡¡probando Git!!" );
-Consola.Print( "Parece que funciona y es una herramienta útil." );
+Console.WriteLine( "Este programa solo tiene interfaz de consola." );
+Console.WriteLine( "Hola desde Rider, ¡¡probando Git!!" );
+Console.WriteLine( "Parece que funciona y es una herramienta útil." );
