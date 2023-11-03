3a8897caea43abe3d81dad26503c70b4b706200f
Baltasar <baltasarq@gmail.com>
2023-11-03 12:09:16 +0100

    Introduce una lambda

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 using Prueba_Git;
 
-Consola.Print( "Este programa solo tiene interfaz de consola." );
-Consola.Print( "Hola desde Rider, ¡¡probando Git!!" );
-Consola.Print( "Parece que funciona y es una herramienta útil." );
+var Muestra = (string m) => Consola.Print( m );
+
+Muestra( "Este programa solo tiene interfaz de consola." );
+Muestra( "Hola desde Rider, ¡¡probando Git!!" );
+Muestra( "Parece que funciona y es una herramienta útil." );
