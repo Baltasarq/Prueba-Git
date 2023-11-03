f2c57087cd735c76c0e1b28fd033e85cf6dcde42
Baltasar García Perez-Schofield <baltasarq@gmail.com>
2023-11-03 11:49:13 +0100

    Update Program.cs
    Añade la apertura de exclamación.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
 
 Console.WriteLine( "Este programa solo tiene interfaz de consola." );
-Console.WriteLine( "Hola desde Rider, probando Git!!" );
+Console.WriteLine( "Hola desde Rider, ¡¡probando Git!!" );
 Console.WriteLine( "Parece que funciona y es una herramienta útil." );
